46c58747d59e18d34e7062089eabc772a87ef35e
mmirzabeygi <mohamadmirzabeygi81@gmail.com>
2026-08-18 23:50:14 +0330

    feat(transactions): add transfer endpoint
    Mount POST /api/transfers/ so authenticated users can move funds
    between wallets through the transfer service.

--- ledger/transactions/apis/__init__.py
+++ ledger/transactions/apis/__init__.py
@@ -1,3 +1,4 @@
+from ledger.transactions.apis.transfers.transfer_api import TransferApi
 from ledger.transactions.apis.wallets.credit_decrease.credit_decrease_api import (
     WalletCreditDecreaseApi,
 )
@@ -6,6 +7,7 @@ from ledger.transactions.apis.wallets.credit_increase.credit_increase_api import
 )
 
 __all__ = [
+    "TransferApi",
     "WalletCreditDecreaseApi",
     "WalletCreditIncreaseApi",
 ]
